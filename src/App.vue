e004d55aed752b6311b9d8926290da096e67e713
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-09-04 20:05:40 -0500

    feat(App): added primevue card

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,26 @@
 </script>
 
 <template>
-  <header>
-  </header>
+  <header></header>
 
   <main>
+    <pv-card>
+      <template #header>
+        <img alt="user header" src="./assets/images/profile-pic.jpeg">
+      </template>
+      <template #title>
+        Advanced Card
+      </template>
+      <template #content>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam
+        deserunt
+        quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+      </template>
+      <template #footer>
+        <pv-button icon="pi pi-check" label="Save"/>
+        <pv-button icon="pi pi-times" label="Cancel" class="p-button-secondary" style="margin-left: .5em"/>
+      </template>
+    </pv-card>
   </main>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
-import './assets/main.css'
+//import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import Aura from '@primevue/themes/aura';
 import PrimeVue from 'primevue/config'
+import Card from "primevue/card";
+import Button from "primevue/button";
 
 const app = createApp(App);
+app.use(PrimeVue, {
+    ripple: true, // enable ripple effect
+    theme: {
+        preset: Aura,
+        options:{
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+});
+app.component('pv-card', Card);
+app.component('pv-button', Button);
 
-app.use(PrimeVue, {ripple: true});
 app.mount('#app');
